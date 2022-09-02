fd19458bfd7c84db9de70b2e12277fa081b1817b
Lucas Malandrino <lucas.malandrino@gmail.com>
2022-09-03 00:12:45 +0200

    Moved to arduino-hal

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,17 @@
 #![no_std]
 #![no_main]
 
-use atmega_hal::{usart::BaudrateExt, Peripherals, Usart};
-use avr_hal_generic::prelude::*;
+use arduino_hal::{prelude::*, Peripherals};
 use core::panic::PanicInfo;
 use ufmt::uWrite;
 
-type DefaultClock = atmega_hal::clock::MHz16;
-
-#[avr_device::entry]
+#[arduino_hal::entry]
 unsafe fn main() -> ! {
     let dp = Peripherals::take().unwrap();
-    let pins = atmega_hal::pins!(dp);
+    let pins = arduino_hal::pins!(dp);
 
-    let mut led = pins.pa4.into_output();
-
-    let pd0 = pins.pd0.into_pull_up_input();
-    let pd1 = pins.pd1.into_output();
-
-    let mut serial = atmega_hal::Usart::<_, _, _, DefaultClock>::new(
-        dp.USART0,
-        pd0,
-        pd1,
-        57_600.into_baudrate(),
-    );
-
-    let mut delay = atmega_hal::delay::Delay::<DefaultClock>::new();
+    let mut led = pins.ext_a4.into_output();
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57_600);
 
     loop {
         led.set_high();
@@ -34,7 +20,7 @@ unsafe fn main() -> ! {
         serial.flush();
 
         led.set_low();
-        delay.delay_ms(500u16);
+        arduino_hal::delay_ms(500);
     }
 }
 
@@ -43,14 +29,9 @@ fn panic_handler(p: &PanicInfo) -> ! {
     avr_device::interrupt::disable();
 
     let dp = unsafe { Peripherals::steal() };
-    let pins = atmega_hal::pins!(dp);
+    let pins = arduino_hal::pins!(dp);
 
-    let mut serial = Usart::<_, _, _, DefaultClock>::new(
-        dp.USART0,
-        pins.pd0,
-        pins.pd1.into_output(),
-        57_600.into_baudrate(),
-    );
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57_600);
 
     serial.write_str("Panicked").void_unwrap();
     if let Some(location) = p.location() {
